451ab47f42b5a2c22549a898d0187b89eb9d49fe
gitaepark97 <parkkitae7@gmail.com>
2022-03-23 23:10:02 +0900

    E2E Testing

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common'
 import { CreateMovieDto } from './dto/create-movie.dto'
 import { UpdateMovieDto } from './dto/update-movie.dto'
 import { Movie } from './entities/movie.entity'
@@ -33,7 +33,7 @@ export class MoviesController {
     return this.moviesService.deleteOne(movieId)
   }
 
-  @Put(':id')
+  @Patch(':id')
   patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData)
   }
